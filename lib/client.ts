567975fd28216b87d7d2473994e873fda0f745f6
crocodilelurker <swagatsahu556@gmail.com>
2026-01-21 04:48:28 +0530

    REPLACE

--- lib/client.ts
+++ lib/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from '@elysiajs/eden'
 import type { app } from '../app/api/[[...slugs]]/route'
 
-export const client = treaty<typeof app>('https://rec-gules.vercel.app/').api
+export const client = treaty<typeof app>('https://rec-red.vercel.app/').api
